8048af208208905eba0e7a05d45d6cc32eced712
pascallapointe <3847945+pascallapointe@users.noreply.github.com>
2022-09-27 16:31:46 -0400

    Change application name and memory limit for PM2.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: 'api.pasklab.com',
+      name: 'api.adastry.io',
       script: 'node dist/src/main.js',
       // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '2000M',
+      max_memory_restart: '6G',
       env: {
         NODE_ENV: 'production',
       },
